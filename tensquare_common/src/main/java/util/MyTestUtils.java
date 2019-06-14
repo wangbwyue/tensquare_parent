3109dc824f495290fdba1eb54809308e07b93617
wangbwyue <wang1985yue@163.com>
2019-06-14 17:20:55 +0800

    新增测试工具类

---
+++ tensquare_common/src/main/java/util/MyTestUtils.java
@@ -0,0 +1,14 @@
+package util;
+
+/**
+ * @描述
+ * @创建人 wangyue
+ * @创建时间2019/6/1417:19
+ */
+
+public class MyTestUtils {
+
+    public static void main(String[] args) {
+        System.out.println("==========================");
+    }
+}
