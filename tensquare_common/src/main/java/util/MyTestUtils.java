f8ff9cab308154573d95a625b982cff143c45593
wang1985yue <35571839+wangbwyue@users.noreply.github.com>
2019-06-14 17:24:13 +0800

    Update MyTestUtils.java

--- tensquare_common/src/main/java/util/MyTestUtils.java
+++ tensquare_common/src/main/java/util/MyTestUtils.java
@@ -9,6 +9,6 @@ package util;
 public class MyTestUtils {
 
     public static void main(String[] args) {
-        System.out.println("==========================");
+        System.out.println("=========123123123=================");
     }
 }
