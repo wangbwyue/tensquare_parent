89974281cbf8dbe290af752c024d8f9d3be05d9b
wangbwyue <wang1985yue@163.com>
2019-06-17 08:54:01 +0800

    新增测试工具类1

---
+++ tensquare_common/src/main/java/util/Teee.java
@@ -0,0 +1,13 @@
+package util;
+
+/**
+ * @描述
+ * @创建人 wangyue
+ * @创建时间2019/6/178:47
+ */
+
+public class Teee {
+    public static void main(String[] args) {
+        System.out.println("==================");
+    }
+}
